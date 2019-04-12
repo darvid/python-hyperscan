45c4b229ea327a9f2bca762ef0104fcbf4afe52a
David Gidwani <david.gidwani@gmail.com>
2019-04-12 19:52:38 -0400

    fixed build_wheels script

--- .semaphore/build_wheels.sh
+++ .semaphore/build_wheels.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e -x
 
+mkdir -p /tmp/wheelhouse
+
 for py in /opt/python/*/bin; do
   "${py}/pip" install -UI poetry setuptools
   "${py}/pip" wheel \
@@ -8,12 +10,12 @@ for py in /opt/python/*/bin; do
     -r /io/docs/requirements.txt \
     -w /io/wheelhouse/
   "${py}/poetry" -vvv build
-  mv /io/dist/*.whl /io/wheelhouse/
+  mv /io/dist/*.whl /tmp/wheelhouse/
 done
 
 for whl in /tmp/wheelhouse/*.whl; do
   auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
-mkdir -p /io/dist/
+mkdir -p /io/dist
 cp /io/wheelhouse/hyperscan*.whl /io/dist/
