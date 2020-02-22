d0d4b49360908ce93b80d00a06a6a63bfbcf0f8f
David Gidwani <david.gidwani@gmail.com>
2020-02-22 00:49:48 -0500

    handle zipp take two

--- .semaphore/build_wheels.sh
+++ .semaphore/build_wheels.sh
@@ -4,7 +4,8 @@ mkdir -p /tmp/wheelhouse
 
 for py in /opt/python/*/bin; do
   [[ "$py" == *"cp27"* ]] || [[ "$py" == *"cp34"* ]] && continue
-  "${py}/pip" install -UI poetry setuptools
+  # https://gitlab.com/python-devs/importlib_metadata/issues/107
+  "${py}/pip" install -UI poetry setuptools "zipp<2"
   "${py}/pip" wheel \
     -r /io/tests/requirements.txt \
     -w /io/wheelhouse/
