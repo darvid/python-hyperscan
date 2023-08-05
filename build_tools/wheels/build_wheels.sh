472bc99307e54028cda10be7d560db7c75e30e65
David Gidwani <david.gidwani@atomweight.io>
2023-08-05 12:43:23 -0400

    ci: :ferris_wheel: fix musllinux platform ids and invoke semantic-release manually

--- build_tools/wheels/build_wheels.sh
+++ build_tools/wheels/build_wheels.sh
@@ -4,15 +4,15 @@ set -euxo pipefail
 # Only applicable to macOS builds
 HYPERSCAN_VERSION=${HYPERSCAN_VERSION:-v5.4.2}
 HYPERSCAN_BUILD_TYPE=${HYPERSCAN_BUILD_TYPE:-Release}
-PCRE_VERSION="${PCRE_VERSION:-10.42}"
+PCRE_VERSION="${PCRE_VERSION:-8.45}"
 
 if [[ $(uname) == "Darwin" ]]; then
   export MACOSX_DEPLOYMENT_TARGET="10.14"
   mkdir -p /tmp/hyperscan
   cd /tmp/hyperscan
   brew install boost cmake git pkg-config python@3.11 ragel wget
-  wget -qO- https://github.com/PCRE2Project/pcre2/releases/download/pcre2-${PCRE_VERSION}/pcre2-${PCRE_VERSION}.tar.gz | tar xvz
-  cd "pcre2-${PCRE_VERSION}"
+  wget -qO- https://sourceforge.net/projects/pcre/files/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.gz/download | tar xvz
+  cd "pcre-${PCRE_VERSION}"
   ./configure --prefix=/usr/local --enable-unicode-properties --enable-utf
   nproc=$(sysctl -n hw.logicalcpu)
   make -j${nproc} && make install
