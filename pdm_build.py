d73d9c167e5095ee091d40bfb13694fd0aa589bf
David Gidwani <david.gidwani@atomweight.io>
2023-08-05 11:26:10 -0400

    build: :package: fix build

--- pdm_build.py
+++ pdm_build.py
@@ -14,11 +14,12 @@ hyperscan_ext = Extension(
     include_dirs=[hs_cflags[2:], get_python_inc(plat_specific=1)],
     libraries=["hs", ":libchimera.a", "m", "stdc++"],
     library_dirs=["/opt/hyperscan/lib64", "/usr/lib/x86_64-linux-gnu"],
+    extra_compile_args=["-O0", "-DPCRE_STATIC"],
+    extra_link_args=["-l:libhs.a", "-l:libchimera.a"],
     extra_objects=[
         os.path.join(pcre_path, "libpcre.a"),
         *glob.glob(os.path.join(pcre_path, '*.o')),
     ],
-    runtime_library_dirs=["src/lib"],
 )
 
 
