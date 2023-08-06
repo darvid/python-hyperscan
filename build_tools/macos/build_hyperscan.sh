42a94e36838dd0fc0f15ad29dbc47287b5ec9c18
David Gidwani <david.gidwani@atomweight.io>
2023-08-05 23:10:17 -0400

    build: :package: fix macos build, update manylinux images

---
+++ build_tools/macos/build_hyperscan.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+PCRE_VERSION=${PCRE_VERSION:-8.45}
+
+if pkg-config --validate libhs; then
+  echo "✅ libhs found, nothing to do"
+  exit 0
+fi
+
+if [ -z "$HYPERSCAN_VERSION" ]; then
+  >&2 echo "HYPERSCAN_VERSION must be set"
+  exit 1
+fi
+
+cd /tmp
+git clone -b "${HYPERSCAN_VERSION}" https://github.com/01org/hyperscan.git
+brew install boost cmake git pkg-config python@3.11 ragel wget
+cd hyperscan
+
+# build and install PCRE (static required for Chimera)
+wget -qO- https://sourceforge.net/projects/pcre/files/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.gz/download | tar xvz
+cd "pcre-${PCRE_VERSION}"
+./configure --prefix=/opt/pcre --enable-unicode-properties --enable-utf
+nproc=$(sysctl -n hw.logicalcpu)
+make -j${nproc} && sudo make install
+
+# build and install Hyperscan
+cd /tmp/hyperscan
+cmake \
+  -B build \
+  -S . \
+  -DCMAKE_INSTALL_PREFIX=/opt/hyperscan \
+  -DBUILD_STATIC_AND_SHARED=ON \
+  -DCMAKE_BUILD_TYPE=${HYPERSCAN_BUILD_TYPE} \
+  -DPCRE_SOURCE=/tmp/hyperscan/pcre-${PCRE_VERSION} \
+  -DCMAKE_C_FLAGS="-fPIC" \
+  -DCMAKE_CXX_FLAGS="$CFLAGS -D_GLIBCXX_USE_CXX11_ABI=0"
+cmake --build build --parallel ${nproc}
+sudo cmake --install build
--- build_tools/wheels/build_wheels.sh
+++ build_tools/wheels/build_wheels.sh
@@ -8,31 +8,9 @@ PCRE_VERSION="${PCRE_VERSION:-8.45}"
 
 if [[ $(uname) == "Darwin" ]]; then
   export MACOSX_DEPLOYMENT_TARGET="10.14"
-  mkdir -p /tmp/hyperscan
-  cd /tmp/hyperscan
-  brew install boost cmake git pkg-config python@3.11 ragel wget
-  wget -qO- https://sourceforge.net/projects/pcre/files/pcre/${PCRE_VERSION}/pcre-${PCRE_VERSION}.tar.gz/download | tar xvz
-  cd "pcre-${PCRE_VERSION}"
-  ./configure --prefix=/usr/local --enable-unicode-properties --enable-utf
-  nproc=$(sysctl -n hw.logicalcpu)
-  make -j${nproc} && make install
-
-  cd /tmp
-  mkdir -p build
-  git clone -b ${HYPERSCAN_VERSION} https://github.com/01org/hyperscan.git
-  export CFLAGS="-fPIC"
-  export CXXFLAGS="$CFLAGS -D_GLIBCXX_USE_CXX11_ABI=0"
-  cd hyperscan/build
-  cmake \
-    -DCMAKE_INSTALL_PREFIX=/opt/hyperscan \
-    -DFAT_RUNTIME=ON \
-    -DBUILD_STATIC_AND_SHARED=ON \
-    -DCMAKE_BUILD_TYPE=${HYPERSCAN_BUILD_TYPE} \
-    -DPCRE_SOURCE=../pcre2-${PCRE_VERSION} \
-    -DCMAKE_C_FLAGS="${CFLAGS}" \
-    -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
-    ../
-  make -j${nproc} && make install
+  export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-/usr/local/lib}"
+  export PCRE_PATH="${PCRE_PATH:-/tmp/hyperscan/pcre-${PCRE_VERSION}/.libs}"
 fi
 
+cd "${GITHUB_WORKSPACE}"
 pdm run cibuildwheel --output-dir wheelhouse
