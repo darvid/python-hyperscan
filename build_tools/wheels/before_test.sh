4ee4e0d59938dcd025d18fbd631155545ce609e7
David Gidwani <david.gidwani@atomweight.io>
2023-08-05 23:10:17 -0400

    build: :package: fix macos build, update manylinux images

--- build_tools/wheels/before_test.sh
+++ build_tools/wheels/before_test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
+export PDM_HOME=${PDM_HOME:-/tmp/pdm}
+export PDM_SKIP_ADD_TO_PATH=${PDM_SKIP_ADD_TO_PATH:-true}
+export PATH="${PDM_HOME}/bin:${PATH}"
 curl -sSL https://pdm.fming.dev/install-pdm.py | python -
-export PATH=/root/.local/bin:$PATH
-pdm install -g --no-self --no-lock -p /project -G test
+pdm export -G test -f requirements -o test_requirements.txt
+pip install -r test_requirements.txt
--- setup.py
+++ setup.py
@@ -1,26 +1,62 @@
 import glob
 import os
+import os.path
 import subprocess
+import sys
 from distutils.sysconfig import get_python_inc
 
 from setuptools import Extension, setup
 
-pcre_path = os.getenv("PCRE_PATH", "/opt/pcre/.libs")
-hs_cflags = subprocess.getoutput("pkg-config --cflags libhs").strip()
 
-hyperscan_ext = Extension(
-    "hyperscan._ext",
-    sources=["src/hyperscan/hyperscanmodule.c"],
-    include_dirs=[hs_cflags[2:], get_python_inc(plat_specific=1)],
-    libraries=["hs", ":libchimera.a", "m", "stdc++"],
-    library_dirs=["/opt/hyperscan/lib64", "/usr/lib/x86_64-linux-gnu"],
-    extra_compile_args=["-O0", "-DPCRE_STATIC"],
-    extra_link_args=["-l:libhs.a", "-l:libchimera.a"],
-    extra_objects=[
-        os.path.join(pcre_path, "libpcre.a"),
-        *glob.glob(os.path.join(pcre_path, '*.o')),
-    ],
-)
+def _pkgconfig(args):
+    return subprocess.getoutput(f"pkg-config {args}").strip()
 
 
-setup(ext_modules=[hyperscan_ext])
+def _pkgconfig_get_cflags(lib):
+    return _pkgconfig(f"--cflags {lib}")
+
+
+def _pkgconfig_get_libdir(lib):
+    return _pkgconfig(f"--variable=libdir {lib}")
+
+
+def get_platform_specific_options():
+    if sys.platform == "win32":
+        raise RuntimeError("win32 not currently supported")
+
+    hs_cflags = _pkgconfig_get_cflags("libhs")
+    pcre_libdir = _pkgconfig_get_libdir("libpcre")
+    hs_libdir = _pkgconfig_get_libdir("libhs")
+    pcre_static_libs = [
+        os.path.join(pcre_libdir, "libpcre.a"),
+        *glob.glob(os.path.join(pcre_libdir, '*.o')),
+    ]
+    ext_kwargs = {
+        "extra_objects": pcre_static_libs,
+        "include_dirs": [hs_cflags[2:], get_python_inc(plat_specific=1)],
+        "libraries": ["m", "stdc++"],
+    }
+
+    hs_static_libs = [
+        os.path.join(hs_libdir, lib) for lib in ["libhs.a", "libchimera.a"]
+    ]
+
+    ext_kwargs["extra_objects"] = [
+        *hs_static_libs,
+        *ext_kwargs["extra_objects"],
+    ]
+    return ext_kwargs
+
+
+if __name__ == "__main__":
+    setup(
+        ext_modules=[
+            Extension(
+                "hyperscan._ext",
+                sources=["src/hyperscan/hyperscanmodule.c"],
+                library_dirs=os.getenv("LIBRARY_PATH", "").split(":"),
+                extra_compile_args=["-O0", "-DPCRE_STATIC"],
+                **get_platform_specific_options(),
+            )
+        ]
+    )
